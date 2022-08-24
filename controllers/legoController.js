db6d3aa867cbcf536233e25f733459ee07e1daa9
Fernando Bustillos <fernando@Fernandos-MBP.fios-router.home>
2022-08-23 23:01:30 -0400

    update

--- controllers/legoController.js
+++ controllers/legoController.js
@@ -4,6 +4,15 @@ const Lego = require("../models/Lego.js");
 
 const db = require("../models");
 
+//Authentication
+const authRequired = (req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.send("You need to log in to do that");
+  }
+};
+
 // router.get("/", (req, res) => {
 //   db.Lego.find().then((results) => {
 //     const context = { allLego: results };
@@ -11,7 +20,7 @@ const db = require("../models");
 //   });
 // });
 
-// //Index
+// //INDEX
 router.get("/", async (req, res) => {
   console.log("before");
   let lego = await Lego.find({});
@@ -19,12 +28,12 @@ router.get("/", async (req, res) => {
   res.render("index.ejs", { lego });
 });
 
-//New
+//NEW
 router.get("/new", (req, res) => {
   res.render("new.ejs");
 });
 
-//Show
+//SHOW
 router.get("/:id", async (req, res) => {
   const lego = await Lego.findById(req.params.id);
   res.render("show.ejs", {
@@ -39,5 +48,54 @@ router.get("/:id", async (req, res) => {
 //     });
 //   });
 // });
+
+//CREATE
+router.post("/", (req, res) => {
+  if (req.body.retired === "on") {
+    req.body.retired = true;
+  } else {
+    req.body.retired = false;
+  }
+  Lego.create(req.body, (error, createLego) => {
+    if (error) {
+      console.log("error", error);
+      res.send(error);
+    } else {
+      res.redirect("/lego");
+    }
+  });
+});
+
+//DESTROY
+router.delete("/:id", (req, res) => {
+  Lego.findByIdAndRemove(req.params.id, (err, data) => {
+    if (err) console.log(err);
+    res.redirect("/lego");
+  });
+});
+
+//EDIT
+router.get("/:id/edit", authRequired, (req, res) => {
+  Lego.findById(req.params.id, (err, foundLego) => {
+    res.render("edit.ejs", { lego: foundLego });
+  });
+});
+
+//UPDATE
+router.put("/id", (req, res) => {
+  if (req.body.retired === "on") {
+    req.body.retired = true;
+  } else {
+    req.body.retired = false;
+  }
+  Lego.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updateModel) => {
+      res.redirect("/lego");
+    }
+  );
+});
 
 module.exports = router;
